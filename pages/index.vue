b9d711c0bdf4beb710c2021c5b88ef09844d6a49
Eri Vera <59424621+eriverec@users.noreply.github.com>
2020-10-09 10:02:44 -0500

    initial

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,42 @@
 <template>
-  <div class="container">
-    <div>
-      <Menu />
-      <h1 class="title">
-        cv-nuxt
-      </h1>
-       <button class="uk-button uk-button-primary">Primary</button>
+  <section class="section">
+    <div class="container">
+      <div class="columns">
+        <div class="column is-offset-2 is-8">
+          <h1 class="title is-2">Latest Posts</h1>
+          <hr>
+
+          <h2
+            class="title is-4"
+            v-for="(blogPost, index) in blogPosts"
+              :key="index">
+            <nuxt-link :to="blogPost.fields.slug">
+              {{ blogPost.fields.title }}
+            </nuxt-link>
+          </h2>
+        </div>
+      </div>
     </div>
-  </div>
+  </section>
 </template>
 
 <script>
-export default {}
+import client from '~/plugins/contentful';
+export default {
+  asyncData({ params }) {
+    return client
+      .getEntries({
+        content_type: 'blogPost',
+      })
+      .then(entries => {
+        return { blogPosts: entries.items };
+      })
+      .catch(e => console.log(e));
+  },
+  head: {
+    title: 'Latest Posts',
+  },
+};
 </script>
 
 <style>
